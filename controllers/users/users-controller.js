220dd189cbe3cf99577875ac11393bcbd4549d21
Wzcjoyce <93357863+Wzcjoyce@users.noreply.github.com>
2022-11-13 18:29:05 -0500

    section 5 complete

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -5,6 +5,8 @@ const UserController = (app) => {
     app.get('/api/users', findUsers);
     app.get('/api/users/:uid', findUserById);
     app.post('/api/users', createUser);
+    app.delete('/api/users/:uid', deleteUser);
+    app.put('/api/users/:uid', updateUser);
 }
 
 const findUsers = (req, res) => {
@@ -32,6 +34,26 @@ const createUser = (req, res) => {
     users.push(newUser);
     res.json(newUser);
 }
+
+const deleteUser = (req, res) => {
+    const userId = req.params['uid'];
+    users = users.filter(usr =>
+        usr._id !== userId);
+    res.sendStatus(200);
+}
+
+
+const updateUser = (req, res) => {
+    const userId = req.params['uid'];
+    const updates = req.body;
+    users = users.map((usr) =>
+        usr._id === userId ?
+            {...usr, ...updates} :
+            usr
+    );
+    res.sendStatus(200);
+}
+
 
 
 
